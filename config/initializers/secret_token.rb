bc62e654cde96e84350ea7191b421d9cc06ac561
smarsh25 <sjm.atl@gmail.com>
2014-02-02 12:50:13 -0800

    base setup, first commit (included bcrypt and bootstrap)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musiq::Application.config.secret_key_base = '4062d42d1f1f8a7b5fa292596df29e6bc50dbd5b92efce47ca6bf908b626b4a4ac9d978bb88f98bc824e0b6a84ccdbdd36ecec35ab0af9de892503a9d6c43a87'
