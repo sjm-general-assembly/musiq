77f55251b39e7ddd428b59d0b4fcceada3939960
smarsh25 <sjm.atl@gmail.com>
2014-02-03 13:45:06 -0800

    adding delete user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,41 @@ class UsersController < ApplicationController
 	# allow only signed in users to edit/update profile
   # before_filter :signed_in_user, only: [:update]
 
+  # GETs a form to show user profile
   def show
     @user = User.find(params[:id])
   end
 
+  # GETs a form to edit user profile
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # GETs a form to create a new user
   def new
     @user = User.new()
   end
 
+  # POSTs the results from new, creating a new user
   def create
     new_user = params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    @user=User.new(new_user)
+    @user = User.new(new_user)
     if @user.save
-      flash[:success] = "Welcome to Ritly!"
+      flash[:success] = "Welcome to MusiQ!"
       sign_in @user
-      redirect_to @user
+      redirect_to root_url
     else
       render'new'
     end
   end
 
-  # Not used (but could provide update functionality)
-  # def update
-  #   @user = User.find(params[:id])
-  #   @user.update_attributes(params[:user])
-  #   render :show
-  # end
+  # DELETE destroy the current user
+  def destroy
+    user = User.find(params[:id])
+    sign_out
+    user.destroy      
+    redirect_to root_url
+  end
+
+
 end
