65f9b8678e393f3edc9f32c5498ab8e771eb17d1
smarsh25 <sjm.atl@gmail.com>
2014-02-06 17:02:54 -0800

    adding seed data (2 more users), made img responsive

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,6 @@ Song.delete_all
 Track.delete_all
 
 f = User.create(:name => 'Fred', :email => "fred@gmail.com", :password => "asdasd", :password_confirmation => "asdasd", :role => 'listener')
+j = User.create(:name => 'Joe', :email => "joe@gmail.com", :password => "asdasd", :password_confirmation => "asdasd", :role => 'listener')
 sm = User.create(:name => 'Steve', :email => "steve@gmail.com", :password => "stevem", :password_confirmation => "stevem", :role => 'admin')
+nk = User.create(:name => 'Nikki', :email => "nikki@gmail.com", :password => "niknaks", :password_confirmation => "niknaks", :role => 'admin')
